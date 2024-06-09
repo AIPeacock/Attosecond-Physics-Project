443d3ec0a710391e9724d3f061b77a79fd49ab95
Aaron Peacock <aaronpeacock1@outlook.com>
2024-06-09 12:37:55 +0100

    Edits

--- WaveEQ_MK/DipoleMom2.py
+++ WaveEQ_MK/DipoleMom2.py
@@ -4,8 +4,8 @@ import matplotlib.pyplot as plt
 from scipy import integrate
 from scipy.integrate import solve_ivp
 
-def Field(t,E0): 
-    return np.cos(w*t)*(E0)
+def Field(t,F): 
+    return np.cos(w*t)*F
 
 def hydroDTME(p,k):              # K is ionisation potential(Sure ? ), p is momentum
      DTME = (1j*8*((2*(k**5))**(1/2))*p)/(np.pi*(((p**2)+(k**2))**3))
@@ -14,15 +14,14 @@ def hydroDTME(p,k):              # K is ionisation potential(Sure ? ), p is mome
 w = 0.057              #Define frequency of field
 t0 = 0                 #Define intial time (t_ref)
 tf = (2*np.pi/w)       #Define final time 
-dt = 0.1                 #Time steps
+dt = 0.5                 #Time steps
 I0 = (1*(10**14))/(3.51*(10**16))
 E0 = I0**(1/2)
 Ip=15.7/27.2
 
-t_full=np.arange(t0,tf -dt,dt)              # Time for plotting, has -dt as needs to match length of Dipole
+t_full=np.arange(t0,tf-dt,dt)              # Time for plotting, has -dt as needs to match length of Dipole
 Dipole=[]
 t_list=[]
-
                                                          # Are these in atomic units
 Up = (E0**2)/(4*(w**2))                                    #Ponderomotive force/potential
 gamma = (Ip/2*Up)**(1/2)                                 # Keldysh parameter
@@ -42,12 +41,13 @@ for i in range(0,len(t_list)):                           # Main dipole calc
     #print(i)
     if t_list[i][0] == t_list[i][-1]:
         continue
-    At = integrate.cumulative_trapezoid(Field(t_list[i],E0),dx=dt,initial=0)  # Integrate the field to find Potential, Is it okay to always have inital = 0 even for stepping through fields ? 
+    At = integrate.cumulative_trapezoid(Field(t_list[i],E0),dx=dt,initial=0)
+    #At = solve_ivp(Field,[round(t_list[i][0],6),round(t_list[i][-1],6)],y0=[0],t_eval=t_list[i])  # Integrate the field to find Potential
     A_t = -1*At
     A_t2 = A_t * A_t
-    sol = integrate.trapezoid(A_t,t_list[i],dx=dt)                                                       #Integral for A_t for all times
+    sol = integrate.trapezoid(A_t,t_list[i])                                                       #Integral for A_t for all times
 
-    solsquared = integrate.trapezoid(A_t2,t_list[i],dx=dt)                                               # Integral for A_t^2 for all times
+    solsquared = integrate.trapezoid(A_t2,t_list[i])                                               # Integral for A_t^2 for all times
 
     Ps = -(1/(t_list[i][-1]-t_list[i][0]))*sol                                                      # Calculating stationary momentum
 
@@ -65,7 +65,7 @@ for i in range(0,len(t_list)):                           # Main dipole calc
 
 # np.savetxt('Dipolelist.txt',Dipole)
 
-tmax = ((tf-t0)/dt) -1                                                       
+tmax = ((tf-t0)/dt) -dt                                                        
 i1=0
 i2=0
 i_sum = 0
@@ -88,4 +88,3 @@ plt.plot(freq_axis/w,D_spectrum)
 plt.xlabel('Frequency')
 #plt.xlim(-1,20)
 plt.ylabel('Intensity')
-plt.show()
--- WaveEQ_MK/Test.py
+++ WaveEQ_MK/Test.py
@@ -1,11 +1,22 @@
 import numpy as np 
+from numpy.fft import fft,ifftshift,fftshift,fftfreq
+import matplotlib.pyplot as plt
+from scipy import integrate
+from scipy.integrate import solve_ivp
 
 w = 0.057              #Define frequency of field
 t0 = 0                 #Define intial time (t_ref)
 tf = (2*np.pi/w)       #Define final time 
 dt = 1             #Time steps
+I0 = (1*(10**14))/(3.51*(10**16))
+E0 = I0**(1/2)
 
+def Field(t,F): 
+    return np.sin(w*t)*(F)
+
+t_full=np.arange(t0,tf-dt,dt)
 t_list = []
+t_array = np.zeros((int(tf),int(tf)))
 
 for ti in np.arange(t0,tf,dt):    # Maybe this dt should be bigger and 
 
@@ -18,22 +29,13 @@ for ti in np.arange(t0,tf,dt):    # Maybe this dt should be bigger and
         #np.array(t_list)
         #t_list = np.append(t_list,t_eva)
 
+Field_list = Field(t_full,E0)
 
-t_max = (tf - t0)/dt 
-
-
-
+A_t = -integrate.cumulative_trapezoid(Field_list,t_full,dx=dt,initial=0)
 
 
 
-a = 0
-for i in range(0,len(t_list)):
-    print(i)
-    for ti in np.arange(t0,tf,dt):
-        if  t_list[i][0] == ti:
-            a = a + 1 
-            print('ti',ti)
-            print('t_list',t_list[i][0])
-print('a',a)
+
+
 
         
