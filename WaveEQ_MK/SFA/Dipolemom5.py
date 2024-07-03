ac55305799717c5422183f3366fc6bc95285197a
Aaron Peacock <aaronpeacock1@outlook.com>
2024-07-03 21:02:20 +0100

    Updates to SFA

--- WaveEQ_MK/SFA/Dipolemom5.py
+++ WaveEQ_MK/SFA/Dipolemom5.py
@@ -3,35 +3,37 @@ from numpy.fft import fft, ifftshift, fftshift, fftfreq
 import matplotlib.pyplot as plt
 from scipy import integrate
 import time
+import cProfile
+import pstats
 #%reload_ext line_profiler
 # Function to calculate the electric field at time t
 def Field(t, F, w):
     return np.sin(w * t) * F
 
 def pulse_field(t,F,w,Nc):
-     Carrier = np.sin(w*(t-(Nc*2*np.pi/w)/2))          #Defines the carrier, sin, with centering(t - tmax/2),   Nc*2*np.pi/w gives us the tmax by multiplying the number of cycles by f0 
-     st=(Carrier*((np.sin(np.pi*t/(Nc*2*np.pi/w)))**2))   #Defines the envelope function sinsquared(pi* t/tmax)
-     return st*F
+     #Carrier = np.sin(w*(t-(Nc*2*np.pi/w)/2))           #Defines the carrier, sin, with centering(t - tmax/2),   Nc*2*np.pi/w gives us the tmax by multiplying the number of cycles by f0 
+     #envelope =((np.sin(np.pi*t/(Nc*2*np.pi/w)))**2))   #Defines the envelope function sinsquared(pi* t/tmax)
+     return (np.sin(w*(t-(Nc*2*np.pi/w)/2))*((np.sin(np.pi*t/(Nc*2*np.pi/w)))**2))*F
 
 # Function to calculate the Dipole Transition Matrix Element (DTME)
 def hydroDTME(p, k):
-    DTME = (1j * 8 * np.sqrt(2 * (k**5)) * p) / (np.pi * (((p**2) + (k**2))**3))
-    return DTME
+    return (1j * 8 * np.sqrt(2 * (k**5)) * p) / (np.pi * (((p**2) + (k**2))**3))
 
 def time_to_index(time_value, dt):
-    return int(time_value * (1/dt))
+    return int(time_value/dt)
 
 # Define constants
 w = 0.057  # Frequency of field
 dt = 0.1  # Time steps
 t0 = 0  # Initial time
-Nc = 4
+Nc = 10
 tf =Nc*(2 * np.pi / w)  # Final time
 N = int((tf-t0) / dt)  # Number of time steps/samples
 
 
+
 # Calculate field parameters
-I0 = (1 * (10**14)) / (3.51 * (10**16))  # Intensity
+I0 = (1.5 * (10**14)) / (3.51 * (10**16))  # Intensity
 E0 = np.sqrt(I0)  # Electric field amplitude
 Ip = 15.7 / 27.2  # Ionisation potential
 
@@ -45,8 +47,13 @@ Dipole = []
 Up = (E0**2) / (4 * (w**2))  # Ponderomotive force/potential
 gamma = np.sqrt(Ip / (2 * Up))  # Keldysh parameter
 kappa = np.sqrt(2 * Ip)  # Kappa
-epsilon = 1/((10*kappa)**2)
+epsilon = 1e-4 # 1/((10*kappa)**2)  #Comupational prefactor 
 
+#Calculate Cut-off frequency
+
+w_c= Ip + 3.17*Up
+cutoff = w_c/w
+print('Cutoff (Harmonic Order)',cutoff)
 # Record start time for performance measurement
 time_a = time.time()
 
@@ -64,7 +71,7 @@ print(time_b - time_a)
 At=np.zeros(N+1)
 time_e = time.time()
 for i in np.linspace(t0,tf,N):                               # Pre-calculating the vector potential for all possible times.
-     Atint = (dt/2)*(Field(i,E0,w)+Field(i-1,E0,w))  
+     Atint = (dt/2)*(pulse_field(i,E0,w,Nc)+pulse_field(i-1,E0,w,Nc))  
      index = time_to_index(i,dt)
      At[index] = -1*Atint
 At2 = np.square(At)
@@ -93,46 +100,38 @@ time_c = time.time()
 # Loop over all valid combinations of ionization and recombination times
  
 for ti, tr in zip(ti_list, tr_list):
-    if tr <= ti:
-            continue
-    x = time_to_index(ti,dt)
-    y = time_to_index(tr,dt)
-    #print('Tr',tr,'Ti',ti)
-    #n = int((tr-ti)/dt)                         # Generates an array of times with each element including the increments between ti,tr
-    #print(n)
-    #t_eva = np.linspace(ti, tr, n)
-    #print(t_eva)
-    #A_t = At[x:y]          # x:y is the indexs of time that ti and tr correspond too.  
-     
-    #print('A_t',len(A_t),'A_t2',len(A_t2),'T_eva',len(t_eva))
-    # Integrate A(t) and A(t)^2 over time to get sol and solsquared
-    #sol = integrate.trapezoid(A_t, t_eva)
-    solsum = np.sum(sol1[x:y])
-    #print(sol - solsum)
-    #solsquared = integrate.trapezoid(A_t2, t_eva)
-    solsquaredsum = np.sum(solsquared2[x:y])
+    if (tr-ti) <= dt:
+        #print('Tr = Ti')
+        continue
+    #print(tr-ti)
+    x = time_to_index(ti,dt)                            #Generate index corresponding to ionisation time
+    y = time_to_index(tr,dt)                            #Generate index corresponding to recombination time
     
+    solsum = np.sum(sol1[x:y+1])                          #Access integrals, can these instead be included straight into the calculations to remove uneccessary calculations ? 
+   
+    solsquaredsum = np.sum(solsquared2[x:y+1])
+    delta_t = tr - ti
     # Calculate stationary momentum Ps
-    Ps = -solsum / (tr - ti)
+    Ps = -solsum / delta_t
     
     # Calculate the classical action Sv
-    Sv = Ip * (tr - ti) + 0.5 * (Ps**2) * (tr - ti) + Ps * solsum + 0.5 * solsquaredsum
+    Sv = Ip * delta_t + 0.5 * (Ps**2) * delta_t + Ps * solsum + 0.5 * solsquaredsum
 
     # Calculate ionization and recollision dipole matrix elements
     d_matrix_ion = hydroDTME(Ps + At[x], kappa)
     d_matrix_recol = np.conj(hydroDTME(Ps + At[y], kappa))
     
     # Define prefactors for the calculation
-    prefactor = ((2*np.pi)/(epsilon+1j*(tr-ti)))**(3/2)  # Prefactor from momentum integration using saddle point
+    prefactor = ((2*np.pi)/(epsilon+1j*delta_t))**(3/2)  # Prefactor from momentum integration using saddle point, why does this not work?!?!?!? what am i doing wronmg :(
 
     # Calculate and store the dipole moment
-    Dipole.append((1j * prefactor * pulse_field(ti, E0, w,Nc) * d_matrix_ion*np.exp(-1j * Sv) )* d_matrix_recol )
-    ti_check = ti
+    Dipole.append((1j * prefactor *d_matrix_recol * d_matrix_ion* pulse_field(ti,E0,w,Nc)*np.exp(-1j * Sv)))
+
 
 # Record end time for integration
 time_d = time.time()
-print('Time integration')
-print(time_d - time_c)
+print('Time integration (mins)')
+print((time_d - time_c)/60)
 
 # Convert Dipole list to a NumPy array for efficient computation
 Dipole = np.array(Dipole)
@@ -149,38 +148,46 @@ for i in range(N, 0, -1):
     # print('Splice i1',i1,'i2',i2,'Dipole',Dipole[i1:i2])
     i1 = i2
     
+
 #Plot the field 
-#pulse_field_list = pulse_field(t_full,E0,w,period=2)
-#plt.figure(1)
-#plt.plot(t_full,pulse_field_list)
-
-
 Field_list = pulse_field(t_full,E0,w,Nc)
 plt.figure(2)
-plt.plot(t_full/(2 * np.pi / w),Field_list)
+plt.plot(t_full/(2 * np.pi / w),Field_list/max(Field_list))
 plt.xlabel('Time (Cycles of carrier)')
 
 plt.figure(3)
 field_freq_axis = (2*np.pi)*fftshift(fftfreq(t_full.shape[-1],d=dt))
 Freq_Field = fftshift(fft(Field_list))
-plt.plot(field_freq_axis/w,abs(Freq_Field)**2)
+plt.plot(field_freq_axis/w,abs(Freq_Field)**2/max(abs(Freq_Field)**2))
 plt.xlabel('Frequency (Harmonic order)')
 plt.xlim(-2,2)
 
 # Plot the real part of the dipole moment over time
 plt.figure(4)
-plt.plot(t_full/(2 * np.pi / w), np.real(Dipole_total))
+plt.plot(t_full/(2 * np.pi / w), np.real(Dipole_total)/max(np.real(Dipole_total)))
 plt.xlabel('Time (Cycles of carrier) ')
 plt.ylabel('Dipole Moment')
 
 # Calculate the Fourier transform of the dipole moment
 Dipole_freq = fftshift(fft((Dipole_total)))
-freq_axis = fftshift(fftfreq(t_full.shape[-1], d=dt))
+freq_axis = (2*np.pi)*fftshift(fftfreq(t_full.shape[-1], d=dt))
 D_spectrum = np.abs(Dipole_freq)**2
 
 # Plot the spectrum
-plt.figure(5)
+plt.figure(figsize=(12,12),dpi=120.0)
 plt.semilogy((freq_axis[freq_axis >= 0]) / w, D_spectrum[freq_axis >= 0])
 plt.xlabel('Frequency (Harmonic Order)')
 plt.ylabel('Intensity')
+plt.xlim(0,50)
+plt.show()
+
+#Plot the spectrum
+plt.figure(figsize=(12,12),dpi=120.0)
+plt.semilogy((freq_axis[freq_axis >= 0]) / w, D_spectrum[freq_axis >= 0],marker = 'o')
+plt.xlabel('Frequency (Harmonic Order)')
+plt.ylabel('Intensity')
+plt.xticks(np.arange(0,50,1))
+plt.xlim(0,50)
+plt.vlines(x=[1,3,5,7,9,11,cutoff/2],ymin=0,ymax=1e-4,colors =['red','red','red','red','red','red','green'])
+plt.axvline(x=cutoff,ymin=0,ymax=0.9,color='blue')
 plt.show()
